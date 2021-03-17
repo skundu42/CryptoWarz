7371c11e5b768357a9dba695c3d4d87238ec1810
skundu42 <skundu_be19@thapar.edu>
2021-03-17 09:50:04 +0530

    minor directory changes

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,21 @@
 const path = require("path");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  contracts_directory: './client/src/contracts/',
+  contracts_build_directory: './client/src/abis/',
+  compilers: {
+    solc: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
     }
   }
-};
+}
